95711d40aefa36e1ab73835b8129cd4f5770723a
sanskar-successive <sanskar.mishra@successive.tech>
2023-11-16 13:29:14 +0530

    backend assignment 2

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import { mockData } from './mockData.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(cookieParser())
+
+app.get('/', (req, res)=>{
+    res.send(mockData)
+})
+app.listen(PORT, ()=>{
+    console.log(`server listening on port ${PORT}`)
+})
---
+++ mockData.js
@@ -0,0 +1,14 @@
+export const mockData = [
+  {
+    firstName: "Donald",
+    lastName: "Trump",
+    age: 75,
+    party: "Republican",
+  },
+  {
+    firstName: "Joe",
+    lastName: "Biden",
+    age: 81,
+    party: "Democrats",
+  },
+];
