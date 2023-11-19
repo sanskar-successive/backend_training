15b00beb5a6812a1b5a06b1a75db833aa8d8d850
sanskar-successive <sanskar.mishra@successive.tech>
2023-11-19 12:06:34 +0530

    assignment-3

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import { mockData } from './mockData.js';
+// import express from 'express'
+// import cookieParser from 'cookie-parser'
+// import { mockData } from './mockData.js';
+
+// const app = express();
+// const PORT = 5000;
+
+// app.use(cookieParser())
+
+// app.get('/', (req, res)=>{
+//     res.send(mockData)
+// })
+// app.listen(PORT, ()=>{
+//     console.log(`server listening on port ${PORT}`)
+// })
+
+import express from "express";
+import data from "./dataSeeder.json" assert { type: "json" };
+import fs from "fs";
+import { authenticate } from "./authMiddleWare.js";
 
 const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("hi");
+});
+
+app.use('/data', authenticate)
+
+app.get("/data", (req, res) => {
+//   data.push(req.newData);
+  return res.status(200).json(data);
+});
+
+app.get("/data/:id", authenticate, (req, res) => {
+  const { id } = req.params;
+  const item = data.find((item) => item.id === parseInt(id));
+  if (item) return res.json(item);
+  else return res.status(404).json({ error: "user not found" });
+});
+
+app.post("/data", authenticate, (req, res) => {
+  let newData = req.body;
+  newData = { ...newData, id: data.length + 1 };
+  data.push(newData);
+  fs.writeFile("./dataSeeder.json", JSON.stringify(data), (err) => {
+    console.log("some error occured");
+  });
+  return res.status(201).json(data);
+});
+
 const PORT = 5000;
-
-app.use(cookieParser())
-
-app.get('/', (req, res)=>{
-    res.send(mockData)
-})
-app.listen(PORT, ()=>{
-    console.log(`server listening on port ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
---
+++ src/app.js
@@ -0,0 +1,16 @@
+// import express from 'express'
+// import { dataSeeder } from '../public/js/dataSeeder';
+// const app = express();
+
+// app.get('/', (req,res) => {
+//     res.send('hi')
+// })
+
+// app.post('/dataSeeder', (req,res)=>{
+//     res.send(dataSeeder)
+// })
+
+// const PORT = 5000
+// app.listen(PORT, ()=>{
+//     console.log(`server listening on port ${PORT}`)
+// })
