0ed587318c6c116f4023a92848868ca0b8d38733
sanskar-successive <sanskar.mishra@successive.tech>
2023-11-21 12:00:43 +0530

    assignment 4

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -1,15 +1,14 @@
 import joi from "joi";
+import { userSchema } from "../utils/schema/userSchema.js";
 
-const validateUser = (schema) => {
-  return (req, res, next) => {
-    const user = req.body;
-    const { value, error } = schema.validate(user);
+const validateUser = (req, res, next) => {
+  const user = req.body;
+  const { value, error } = userSchema.validate(user);
 
-    if (error) {
-      return res.status(400).json({ error: "Invalid user" });
-    }
-    next();
-  };
+  if (error) {
+    return res.status(400).json({ error: "Invalid user" });
+  }
+  next();
 };
 
 export default validateUser;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,6 @@ import {
   getAllUsersController,
   getUserByIdController,
 } from "../controllers/userController.js";
-import { userSchema } from "../utils/schema/userSchema.js";
 import authenticateUser from "../middlewares/authMiddleware.js";
 import validateUser from "../middlewares/validationMiddleware.js";
 const router = express.Router();
@@ -12,6 +11,6 @@ const router = express.Router();
 router.get("/show", authenticateUser, getAllUsersController);
 router.get("/show/:id",authenticateUser, getUserByIdController);
 
-router.post("/create", authenticateUser,validateUser(userSchema), createUserController);
+router.post("/create", authenticateUser,validateUser, createUserController);
 
 export default router;
