a66c16a22a3e04077b6882987e7313b6af99797c
sanskar-successive <sanskar.mishra@successive.tech>
2023-11-22 13:08:36 +0530

    assignment 3

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken'
 export const authenticateUser = (req,res,next)=>{
     const token = req.headers.authorization
     if(!token) return res.json({error : "token not provided"})
-
     try{
         const decodedUser = jwt.verify(token, "123");
         req.user = decodedUser;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,8 +5,8 @@ import {
   getUserByIdController,
 } from "../controllers/userController.js";
 import { authenticateUser } from "../middlewares/authMiddleware.js";
-const router = express.Router();
 
+const router = express.Router();
 router.get("/show", authenticateUser, getAllUsersController);
 router.get("/show/:id", authenticateUser, getUserByIdController);
 router.post("/create", authenticateUser, createUserController);
