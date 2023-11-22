7493c1612c9fcd92f38081ae683dead74053c35d
sanskar-successive <sanskar.mishra@successive.tech>
2023-11-22 13:03:49 +0530

    assignment 5

--- src/app.js
+++ src/app.js
@@ -8,19 +8,39 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
+const timeoutFunc = (req, res, next) => {
+  let timeoutId;
+  const promise = new Promise((resolve, reject) => {
+    timeoutId = setTimeout(() => {
+      resolve("taking time to fetch");
+    }, 5000);
+
+    setTimeout(() => {
+      reject("request cannot be fulfilled");
+    }, 1000);
+  });
+  promise
+    .then((data) => res.send(data))
+    .catch((err) => next(createError(408, `${err}, request timeout`)));
+};
+
+app.get("/api/timeout", timeoutFunc);
+
+app.patch("/api/unsupported", (req, res) => {
+  res.send("Not Implemented");
+});
+
 app.use((req, res, next) => {
   next(createError(404, "Invalid route, Page Not Found"));
 });
 
 app.use((err, req, res, next) => {
   console.log(err);
-  if(err.status){
-    res.status(err.status).json({message : err.message})
+  if (err.status) {
+    res.status(err.status).json({ error: err.message });
+  } else {
+    res.status(500).json({ message: "Internal Server Error" });
   }
-  else{
-    res.status(500).json({message : "Internal Server Error"})
-  }
-
 });
 
 const PORT = 5000;
