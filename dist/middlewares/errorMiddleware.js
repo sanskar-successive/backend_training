39dbbc07f184764f272b6676090b15a071f31e0d
sanskar-successive <sanskar.mishra@successive.tech>
2023-12-11 10:34:03 +0530

    removed dist

--- dist/middlewares/errorMiddleware.js
+++
@@ -1,5 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.log("error handler chal rha hai");
-    return res.status(err.status || 500).json({ error: err.message || "internal server error" });
-};
-export default errorHandler;
