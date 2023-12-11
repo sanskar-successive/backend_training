fba4aa0c6230492f09fc11475e08b1cd984c9f1f
sanskar-successive <sanskar.mishra@successive.tech>
2023-12-11 14:42:40 +0530

    assignment 7 self reviewed and updated

--- src/middlewares/dynamicValidation.middleware.ts
+++ src/middlewares/dynamicValidation.middleware.ts
@@ -12,7 +12,6 @@ class DynamicValidationMiddleware {
       const path = req.url;
       const method = req.method;
       const key = `${path.substring(1)} ${method}`;
-      console.log(key);
       if (Object.keys(validationConfig).includes(key)) {
         const schema = validationConfig[key];
         const toValidate = req.body;
